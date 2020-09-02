7288279311568da443cab59fe7d9d1fd72b4c3c8
XinyiY5166 <xvy5166@psu.edu>
2020-09-02 06:20:25 +0000

    word

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ Unit = input("Enter unit in F/f or C/c: ")
 if (Unit == "F" or Unit == "f"):
   print(f"{Temperature}° in Fahrenheit is equivalent to {float((Temperature-32)*5/9)}° Celsius.")
 elif(Unit == "C" or Unit == "c"):
-  print(f"{Temperature}° in Celsius is equivalent to {Temperature*1.8+32}° in Fahrenheit.")
+  print(f"{Temperature}° in Celsius is equivalent to {Temperature*1.8+32}° Fahrenheit.")
 else:
   print(f"Invalid unit({Unit}).")
