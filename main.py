4067affcda275a6c59a6977c4fd5dd683a183256
XinyiY5166 <xvy5166@psu.edu>
2020-09-03 13:32:24 +0000

    comments

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Author: xvy5166@psu.edu
+# Contributor: zfx5078@psu.edu
+# Contributor: svs6959@psu.edu
+# Contributor: jtf5383@psu.edu
+
 Temperature = float(input("Enter temperature: "))
 Unit = input("Enter unit in F/f or C/c: ")
 if Unit == "F" or Unit == "f":
